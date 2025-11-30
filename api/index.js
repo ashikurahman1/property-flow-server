93c553e63b57d1efc4ba98aaa8d887f7e460de6e
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-11-30 23:58:49 +0600

    init the project

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'API working!' });
+});
+
+export const handler = serverless(app);
