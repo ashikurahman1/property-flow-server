d0cd41a24ffbffc5fdebce1afdbdf68693c622bf
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-12-01 01:58:46 +0600

    add: add created user data on DB

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -21,12 +21,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Smart server is running');
-// });
+app.get('/', (req, res) => {
+  res.send('Smart server is running');
+});
 
 app.listen(port, () => {
   console.log(`Smart server is running on port: ${port}`);
 });
 
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // Don't forget to add .js extension to avoid getting module not found error
-import { user } from '../../controllers/user.controller.js';
+import { user } from '../controllers/user.controller.js';
 
 const router = express.Router();
 // Define user route handlers here
